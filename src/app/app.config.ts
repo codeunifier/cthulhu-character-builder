e3429390ce1227f9b2b893663e6b3f9ebde072e1
codeunifier <codeunifier@gmail.com>
2025-04-17 14:25:35 -0700

    prepping to deploy to github pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 
@@ -7,7 +7,7 @@ import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()), // Using hash routing for GitHub Pages compatibility
     provideAnimations(),
     provideHttpClient()
   ]
